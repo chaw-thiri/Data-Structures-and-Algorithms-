00c0133607aa10e47f612970d3105995dc9f2ad1
Chaw Thiri San <113085742+chaw-thiri@users.noreply.github.com>
2024-03-23 02:30:51 +0900

    Create bubble_sort_1.cpp

---
+++ bubble_sort_1.cpp
@@ -0,0 +1,37 @@
+
+// -------------------------------- bubble sort  -----------------
+// time :  O(n^2 ) , n = size of the array.
+#include <iostream>
+
+const int s = 15;
+using namespace std;
+void swap(int& x , int& y );
+
+int main(){
+    int arr[s] = {12,342,543,3,53,2,45,632,53,232,2,4,3,2,8};
+    for (int i = 0; i < s ; i ++){
+        for (int j = 0 ; j < s-i ; j ++){
+            if ( arr[j] < arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    //     cout << "Testing ...";
+    //     for (int i = 0; i < s; i ++){
+    //     cout << arr[i] << " ";
+    // }
+    // cout << endl;
+    }
+    
+    
+    for (int i = 0; i < s; i ++){
+        cout << arr[i] << " ";
+    }
+    
+}
+
+void swap(int& x , int& y ){
+    int temp;
+    temp = x;
+    x =y;
+    y = temp;
+}
